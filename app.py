991d7cd9fffb0165c174eb1e6e90fbd5007efaae
Taimoor Pasha <46814549+taimoorpashanbs17@users.noreply.github.com>
2020-04-14 04:14:55 +0500

    Latest Changes

--- app.py
+++ app.py
@@ -99,11 +99,11 @@ api.add_resource(GetAllPlaylists, '/all_playlists')
 
 
 if __name__ == '__main__':
-    # from db import db
-    # db.init_app(app)
-    #
-    # if app.config['DEBUG']:
-    #     @app.before_first_request
-    #     def create_tables():
-    #         db.create_all()
+    from db import db
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
     app.run(port=5000)
--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from app import app
 
 db = SQLAlchemy()
 
