898165c4acbb2f360b1bc7e502111164855d6151
Taimoor Pasha <46814549+taimoorpashanbs17@users.noreply.github.com>
2020-04-14 15:00:45 +0500

    Little Changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from db import db
 
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh, GetUsers
@@ -19,6 +20,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWTManager(app)
 
@@ -101,11 +106,5 @@ api.add_resource(GetAllPlaylists, '/all_playlists')
 
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
