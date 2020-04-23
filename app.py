15e47e6ae15db26ed0760b150613ab1863f54f4d
Taimoor Pasha <46814549+taimoorpashanbs17@users.noreply.github.com>
2020-04-23 17:30:01 +0500

    Latest Changes

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from db import db
+import os
 
-# import flask_login
-# login_manager = flask_login.LoginManager()
+
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh, \
     GetUsers, UserLogout, RevokedTokenModel, MakeInActive, UpdateUser
@@ -18,14 +18,14 @@ from resources.tracks import NewTrack, Tracks, GetAllTracks, UpdateTrack
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Test@12345@localhost:5432/flask_restapi'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-app.secret_key = 'jose'  # could do app.config['JWT_SECRET_KEY'] if we prefer
+app.secret_key = 'jose'
 api = Api(app)
-# login_manager.init_app(app)
+
 
 @app.before_first_request
 def create_tables():
@@ -42,8 +42,8 @@ one possible use case for claims are access level control, which is shown below.
 
 
 @jwt.user_claims_loader
-def add_claims_to_jwt(identity):  # Remember identity is what we define when creating the access token
-    if identity == "taimoorpasha2009@gmail.com":   # instead of hard-coding, we should read from a config file or database to get a list of admins instead
+def add_claims_to_jwt(identity):
+    if identity == "admin@flask-restapi-postgres.com":
         return {'is_admin': True}
     return {'is_admin': False}
 
